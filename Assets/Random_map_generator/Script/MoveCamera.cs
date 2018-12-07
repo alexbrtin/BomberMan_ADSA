369a1cde29ba7099b60a05903ab9ab69833c6137
GuillaumeeG <38972881+GuillaumeeG@users.noreply.github.com>
2018-12-07 10:50:35 +0100

    Init Project

---
+++ Assets/Random_map_generator/Script/MoveCamera.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour
+{
+    public float vitesse_deplacement = 4f; // Vitesse de deplacement
+   
+    public Vector3 Pos_depart; // Position de départ 
+    private bool deplacement;// boolean pour savoir si la camera est déjà en déplacement 
+
+    
+    void FixedUpdate()// Méthode qui permettra de déplacer la caméra dans la scène 
+    {
+        if (Input.GetMouseButtonDown(1))// Si il y a un clic droit de la souris (enfoncé)
+        {
+            Pos_depart = Input.mousePosition; // On récupère les coordonnées du curseur de la souris dans Pos_depart
+            deplacement = true; // On passe le boolean du deplacement à true
+           
+
+        }
+        if (Input.GetMouseButtonUp(1) && deplacement)// Si il y a un clic droit de la souris (relaché)
+        {
+            deplacement = false; // on arrete passe deplacement à true 
+          
+        }
+        if (deplacement) // Si deplacement est a true 
+        {
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - Pos_depart); // On déplace la souris par rapport à la position
+                                                                                               // initialie et la position ou elle est actuellement 
+         
+            Vector3 move = new Vector3(pos.x * vitesse_deplacement, pos.y * vitesse_deplacement, 0); // Deplacement 
+
+            transform.Translate(move, Space.Self);// On change les coordonnées de la camera 
+            
+        }
+    }
+}
---
+++ Assets/Random_map_generator/Script/PixelPerfectCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PixelPerfectCamera : MonoBehaviour
+{
+     
+    public static float Pixel_sprite = 1f; // = 1 Car on a choisi cette valeur au départ pour les sprites 
+    public static float echelle = 1f; // = 1 Car ils sont tous à la même échelle 
+
+    public Vector2 nativeResolution = new Vector2(160, 144); // Dimension GameBoy 
+  
+    void Awake() // Méthode 
+    {
+        var camera = GetComponent<Camera>(); // On passe la camera en référence de cette variable pour y attacher ce script
+        if (camera.orthographic) // si la camera fonctionne de façon orthogonal ( comme on est en x,y,z)
+        {
+            var dir = Screen.height; // On récupère la taille de l'écran 
+            var res = nativeResolution.y; // Res prend la valeur du y de la resolution native (144) dans notre cas 
+
+            echelle = dir / res; // L'echelle s'adapte aux proportions de notre écran 
+            Pixel_sprite *= echelle; // Pixel proportionnel à l'échelle  
+            camera.orthographicSize = (dir / 2.0f) / Pixel_sprite; 
+        }
+    }
+}
