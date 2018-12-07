8e06591d31cfe9ff285d4888c01c48d81a5c1326
GuillaumeeG <38972881+GuillaumeeG@users.noreply.github.com>
2018-12-07 15:46:44 +0100

    Map_full_carton_zone_spawn
    map juste avec les bords + les cartons partout + la zone de spawn

--- Assets/Random_map_generator/Script/Random_map_tester.cs
+++ Assets/Random_map_generator/Script/Random_map_tester.cs
@@ -9,29 +9,43 @@ public class Random_map_tester : MonoBehaviour
 
     [Space] // Espacement dans l'inspecteur 
     [Header("Visualisation de la Map")] // Noms de l'espace dans l'inspecteur   
-    public GameObject mapContainer;
-    public GameObject tilePrefab;
-    public Vector2 Taille_tuile = new Vector2(16, 16); // taille en pixel d'une tuile 
+    public GameObject mapContainer; // Game object de la map 
+    public GameObject tile_grass; // Prefab du sol de la map 
+    public GameObject Wall_outside_Prefab;
+    public GameObject Wall_inside_Prefab;
+    public GameObject Box_Prefab;
+    public Vector2 Taille_tuile = new Vector2(16, 16); // taille en pixel d'une tuile
+
+    [Space] // Espacement dans l'inspecteur 
+    [Header("Remplissage de la map")] // Noms de l'area  
+    public int pourcentage_Box = 50;
+        
+
+
+
+
+
+     
 
     [Space] // Espacement dans l'inspecteur 
     [Header("Texture de la Map")] // Noms de l'espace dans l'inspecteur   
     public Texture2D Map_Texture; // Texture du gameObject
 
 
-    public Map map; 
+    public Map map;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         map = new Map(); // Instanciation de la map 
 
-	}
-	
-	// Update is called once per frame
-	public void Generer_Map ()
+    }
+
+    // Update is called once per frame
+    public void Generer_Map()
     {
         map.NewMap(LargeurMap, HauteurMap);
-       
+
         Supprimer_Tuile();
         CreateGrid();
     }
@@ -39,50 +53,105 @@ public class Random_map_tester : MonoBehaviour
     {
         Sprite[] Sprites = Resources.LoadAll<Sprite>(Map_Texture.name);// On charche dans le Tableau de Sprite Sprites toutes 
                                                                        // les textures que on a découpé dans l'éditeur
-        var indice_mur_inside = 91;
-        var indice_mur_outside = 75;
+        var indice_mur_inside = 75; // Indice dans mon tableau de sprites des composants de la map 
+        
+        var indice_mur_outside = 91;
+        
         var indice_carton = 90;
+        
         var indice_herbe = 1;
+        
         var indice_terre = 0;
+        
+
+
         var total = map.tuiles.Length; // total prend la valeur de la taille du tableau de tuile dans la classe "map"
         var Max_nb_colonne = map.colonnes; // Nombre de colonne dans notre map
+        var Max_nb_ligne = map.ligne; // Nombre de ligne dans notre map 
         var colonne = 0; // On instancie le nombre de colonne a 0 ( on va parcourir notre map grace à ça ) 
         var ligne = 0; // Même principe que pour les colonnes 
+        var nb_wall_outside = 0; // nombre de mur composant notre map ( pour nommer les game object ) 
+        var nb_box = 0; // nombre de mur composant notre map ( pour nommer les game object ) 
 
         for (int i = 0; i < total; i++) // Pour i allant de 0 à la taille de notre tableau 
         {
-
             colonne = i % Max_nb_colonne; // Colonne prend la valeur de i modulo le nombre maximum de colonne
-                                           
+            var test_ligne = 0;
+            if (colonne > 0)
+            {
+                test_ligne = Max_nb_colonne % colonne;
+            }
 
             var Tuile_X = colonne * Taille_tuile.x; // Position en x de la tuile 
             var Tuile_Y = -ligne * Taille_tuile.y; // Position en y de la tuile 
 
-            var Game_object = Instantiate(tilePrefab); // On instancie un GameObject
-            Game_object.name = "Tuile_ " + i; // Il prendra le nom "tuile" + l'indice dans le tableau 
-            Game_object.transform.SetParent(mapContainer.transform); // On place le game object en tant qu'enfant de la grille dans la hierarchie 
-            Game_object.transform.position = new Vector3(Tuile_X, Tuile_Y, 0); // On change la position actuelle 
+            var grass_dirt_go = Instantiate(tile_grass); // On instancie un GameObject
 
-            var id_sprite = 1; // A modifier 
-            var sprite_render = Game_object.GetComponent<SpriteRenderer> (); // Pour obtenir l'instance du Sprite renderer
-            sprite_render.sprite = Sprites[id_sprite];
+            grass_dirt_go.name = "Tuile_ " + i; // Il prendra le nom "tuile" + l'indice dans le tableau 
+            grass_dirt_go.transform.SetParent(mapContainer.transform); // On place le game object en tant qu'enfant de la grille dans la hierarchie 
+            grass_dirt_go.transform.position = new Vector3(Tuile_X, Tuile_Y, 0); // On change la position actuelle 
+
+            var sprite_render = grass_dirt_go.GetComponent<SpriteRenderer>(); // Pour obtenir l'instance du Sprite renderer
+            sprite_render.sprite = Sprites[indice_herbe];
             if (colonne == (Max_nb_colonne - 1)) // si le numéro de la colonne courante est égales au nombre de colonne de la map 
             {
                 ligne++; // On passe à la ligne suivante 
             }
 
+            if (colonne == 0 || colonne == Max_nb_colonne - 1|| ligne == 0 || ligne == Max_nb_ligne-1)
+            {             
+                var Wall_outside_go = Instantiate(Wall_outside_Prefab);
+                Wall_outside_go.name = "Wall_outside_" + nb_wall_outside;
+                Wall_outside_go.transform.SetParent(mapContainer.transform); // On place le game object en tant qu'enfant de la grille dans la hierarchie 
+                Wall_outside_go.transform.position = new Vector3(Tuile_X, Tuile_Y, 0); // On change la position actuelle                 
+                var Wall_outside_renderer = Wall_outside_go.GetComponent<SpriteRenderer>(); // Pour obtenir l'instance du Sprite renderer
+                Wall_outside_renderer.sprite = Sprites[indice_mur_outside];
+                nb_wall_outside++;
+                map.tuiles[i].mur = true;
+            }
+            if(map.tuiles[i].mur == false && !Spawn(Max_nb_colonne,Max_nb_ligne,ligne,colonne))
+            {
+                var Box_go = Instantiate(Box_Prefab);
+                Box_go.name = "Box_" + nb_box;
+                Box_go.transform.SetParent(mapContainer.transform); // On place le game object en tant qu'enfant de la grille dans la hierarchie 
+                Box_go.transform.position = new Vector3(Tuile_X, Tuile_Y, 0); // On change la position actuelle        
+                var Box_renderer = Box_go.GetComponent<SpriteRenderer>(); // Pour obtenir l'instance du Sprite renderer
+                Box_renderer.sprite = Sprites[indice_carton];
+                nb_box++;
+                map.tuiles[i].carton = true;
+            }
         }
 
     }
+    bool Spawn(int Maxcolonne,int Maxligne,int ligne,int colonne)
+    {
+        bool spawn = false;
+        if((colonne == 1 && ligne == 1) || (colonne == 2 && ligne == 1) || colonne == 1 && ligne == 2)
+        {
+            return true;
+        }
+        if ((colonne == Maxcolonne-2 && ligne == 1) || (colonne == Maxcolonne-3 && ligne == 1) || (colonne == Maxcolonne-2 && ligne == 2))
+        {
+            return true;
+        }
+        if ((colonne == 1 && ligne == Maxligne -2) || (colonne == 2 && ligne == Maxligne-2) || (colonne == 1 && ligne == Maxligne-3))
+        {
+            return true;
+        }
+        if ((colonne == Maxcolonne-2 && ligne == Maxligne-2) || (colonne == Maxcolonne - 3 && ligne == Maxligne - 2) || (colonne == Maxcolonne-2 && ligne == Maxligne-3))
+        {
+            return true;
+        }
+        return spawn;
+    }
+    
     void Supprimer_Tuile()
     {
-
         var enfant = mapContainer.transform.GetComponentsInChildren<Transform>();
         for (var i = enfant.Length - 1; i > 0; i--)
         {
             Destroy(enfant[i].gameObject);
         }
-
     }
 
 }
--- Assets/Random_map_generator/Script/Tile.cs
+++ Assets/Random_map_generator/Script/Tile.cs
@@ -14,6 +14,8 @@ public class Tile
 {
     public int id = 0; // Chaque Tuile (Tile) aura un id propre 
     public Tile[] voisins = new Tile[4];// On aura besoin de connaitre les voisins de la cellule en cours ( 4 voisins direct ) 
+    public bool mur = false;
+    public bool carton = false;
 
 	
 	
